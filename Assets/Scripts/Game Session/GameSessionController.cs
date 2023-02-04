82e2c2b39cc352afb5a70e5bb5b9de9f52acfdeb
Amberfall <91446589+Amberfall@users.noreply.github.com>
2023-02-04 13:55:01 -0500

    adds root depth event listener

--- Assets/Scripts/Game Session/GameSessionController.cs
+++ Assets/Scripts/Game Session/GameSessionController.cs
@@ -5,6 +5,16 @@ using UnityEngine;
 public class GameSessionController : MonoBehaviour
 {
     [SerializeField] private int depthRequiredToWin;
-    
 
+    private CanvasController canvasController;
+
+    private void OnEnable()
+    {
+        canvasController = FindObjectOfType<CanvasController>();
+    }
+
+    private void Start()
+    {
+        canvasController.setDepthRequiredForWin(depthRequiredToWin);
+    }
 }
--- Assets/Scripts/UI/CanvasController.cs
+++ Assets/Scripts/UI/CanvasController.cs
@@ -8,14 +8,21 @@ public class CanvasController : MonoBehaviour
     [SerializeField] TextMeshProUGUI waterRemainingText;
     [SerializeField] TextMeshProUGUI depthGaugeText;
 
+    private int depthRequiredForWin;
+
+    public void setDepthRequiredForWin(int depthRequired)
+    {
+        depthRequiredForWin = depthRequired;
+    }
     public void setWaterRemainingText(object data)
     {
         var waterRemaining = (int)data;
         UnityEngine.Debug.Log("i've been called! water remaining is " + waterRemaining.ToString());
         waterRemainingText.text = waterRemaining.ToString();
     }
-    public void setDepthGuageText(int currentDepth, int depthRequiredForWin)
+    public void setDepthGuageText(object data)
     {
+        int currentDepth = (int)data;
         depthGaugeText.text = currentDepth.ToString() + " / " + depthRequiredForWin.ToString();
     }
 }
